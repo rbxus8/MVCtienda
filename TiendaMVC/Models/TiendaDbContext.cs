04dbd28442c139737507d43e133b783e0efaf00a
julian carvajalino <juliancarvajalino8@gmail.com>
2025-08-18 21:05:21 -0500

    feat(css): diseño borrador de style con layout null

---
+++ TiendaMVC/Models/TiendaDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace TiendaMVC.Models
+{
+    public class TiendaDbContext : DbContext
+    {
+        // Nombre de la cadena de conexión definida en Web.config
+        public TiendaDbContext() : base("name=TiendaConnectionString") { }
+
+        // Representa la tabla de usuarios
+        public DbSet<User> Users { get; set; }
+    }
+}
