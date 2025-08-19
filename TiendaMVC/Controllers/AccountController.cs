82bc42bf9815ec60451382a597d6ab93fc4cce0a
julian carvajalino <juliancarvajalino8@gmail.com>
2025-08-18 19:45:02 -0500

    feat: Login view y cambio en config router para login como pagina principal

---
+++ TiendaMVC/Controllers/AccountController.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Mvc;
+using TiendaMVC.Models;
+
+namespace TiendaMVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: /Account/Login
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        // POST: /Account/Login
+        [HttpPost]
+        public ActionResult Login(User user)
+        {
+            // Validación simple (hardcodeada)
+            if (user.Username == "admin" && user.Password == "1234")
+            {
+                // Redirigir a la página principal
+                return RedirectToAction("Index", "Home");
+            }
+
+            ViewBag.Error = "Usuario o contraseña incorrectos";
+            return View();
+        }
+    }
+}
---
+++ TiendaMVC/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TiendaMVC.Models
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
